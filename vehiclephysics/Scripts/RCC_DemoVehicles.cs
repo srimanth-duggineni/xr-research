1ae3633a2e8c631612153b8af0471229e32108b3
Srimanth Duggineni <srimanthduggineni@Srimanths-MBP.gateway>
2022-02-03 23:16:51 +1100

    XR research essentials

---
+++ vehiclephysics/Scripts/RCC_DemoVehicles.cs
@@ -0,0 +1,33 @@
+﻿//----------------------------------------------
+//            Realistic Car Controller
+//
+// Copyright © 2014 - 2021 BoneCracker Games
+// http://www.bonecrackergames.com
+// Buğra Özdoğanlar
+//
+//----------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RCC_DemoVehicles : ScriptableObject {
+
+	[System.Serializable]
+	public class RCC_DemoVehicle {
+
+		public RCC_CarControllerV3 carController;
+		public bool unlocked = true;
+	
+	}
+
+	public RCC_DemoVehicle[] RCCDemoVehicles;
+
+	public RCC_CarControllerV3[] vehicles;
+
+	#region singleton
+	private static RCC_DemoVehicles instance;
+	public static RCC_DemoVehicles Instance{	get{if(instance == null) instance = Resources.Load("RCC Assets/RCC_DemoVehicles") as RCC_DemoVehicles; return instance;}}
+	#endregion
+
+}
---
+++ vehiclephysics/Scripts/RCC_SkidmarksManager.cs
@@ -0,0 +1,113 @@
+﻿//----------------------------------------------
+//            Realistic Car Controller
+//
+// Copyright © 2014 - 2021 BoneCracker Games
+// http://www.bonecrackergames.com
+// Buğra Özdoğanlar
+//
+//----------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("BoneCracker Games/Realistic Car Controller/Misc/RCC Skidmarks Manager")]
+public class RCC_SkidmarksManager : MonoBehaviour {
+
+	#region singleton
+	private static RCC_SkidmarksManager instance;
+	public static RCC_SkidmarksManager Instance {
+
+		get {
+
+			if (instance == null) {
+
+				instance = FindObjectOfType<RCC_SkidmarksManager>();
+
+				if (instance == null) {
+
+					GameObject sceneManager = new GameObject("_RCC_SkidmarksManager");
+					instance = sceneManager.AddComponent<RCC_SkidmarksManager>();
+
+				}
+
+			}
+
+			return instance;
+
+		}
+
+	}
+
+	#endregion
+
+	public RCC_Skidmarks[] skidmarks;
+	public int[] skidmarksIndexes;
+	private int _lastGroundIndex = 0;
+
+	void Start () {
+		
+		skidmarks = new RCC_Skidmarks[RCC_GroundMaterials.Instance.frictions.Length];
+		skidmarksIndexes = new int[skidmarks.Length];
+
+		for (int i = 0; i < skidmarks.Length; i++) {
+			
+			skidmarks [i] = Instantiate (RCC_GroundMaterials.Instance.frictions [i].skidmark, Vector3.zero, Quaternion.identity);
+			skidmarks [i].transform.name = skidmarks[i].transform.name + "_" + RCC_GroundMaterials.Instance.frictions[i].groundMaterial.name;
+			skidmarks [i].transform.SetParent (transform, true);
+
+		}
+		
+	}
+	
+	// Function called by the wheels that is skidding. Gathers all the information needed to
+	// create the mesh later. Sets the intensity of the skidmark section b setting the alpha
+	// of the vertex color.
+	public int AddSkidMark ( Vector3 pos ,   Vector3 normal ,   float intensity ,   int lastIndex, int groundIndex  ){
+
+		if (_lastGroundIndex != groundIndex){
+
+			_lastGroundIndex = groundIndex;
+			return -1;
+
+		}
+
+		skidmarksIndexes[groundIndex] = skidmarks [groundIndex].AddSkidMark (pos, normal, intensity, lastIndex);
+		
+		return skidmarksIndexes[groundIndex];
+
+	}
+
+	// Function called by the wheels that is skidding. Gathers all the information needed to
+	// create the mesh later. Sets the intensity of the skidmark section b setting the alpha
+	// of the vertex color.
+	public int AddSkidMark ( Vector3 pos ,   Vector3 normal ,   float intensity ,   int lastIndex, int groundIndex, float width){
+
+		if (_lastGroundIndex != groundIndex){
+
+			_lastGroundIndex = groundIndex;
+			return -1;
+
+		}
+
+		skidmarks [groundIndex].markWidth = width;
+		skidmarksIndexes[groundIndex] = skidmarks [groundIndex].AddSkidMark (pos, normal, intensity, lastIndex);
+
+		return skidmarksIndexes[groundIndex];
+
+	}
+
+	public void CleanSkidMark() {
+
+        for (int i = 0; i < skidmarks.Length; i++)
+			skidmarks[i].Clean();
+
+	}
+
+	public void CleanSkidMark(int index) {
+
+		skidmarks[index].Clean();
+
+	}
+
+}
